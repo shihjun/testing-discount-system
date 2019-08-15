6f45234009a16d75642299a942e394c2564c890f
shih_jun_teh <shih_jun_teh@dell.com>
2019-08-15 20:50:57 +0800

    change discount checker function

--- src/main/java/learning/DiscountChecker.java
+++ src/main/java/learning/DiscountChecker.java
@@ -4,26 +4,35 @@ import java.time.LocalDate;
 
 public class DiscountChecker {
 
-	public static int applyDiscount(String discountType, int price, Discount discount, String productName) {
-		if (discountType.equals("absolute")) {
-			return price - discount.absoluteValue;
-		} else if (discountType.equals("percentage")) {
-			return price - (price * discount.absoluteValue / 100);
-		} else if (discountType.equals("prodcutSpecific")) {
-			for (int i = 0; i < discount.applicableProducts.length; i++) {
-				if (discount.applicableProducts[i].equals(productName)) {
-					return price - discount.absoluteValue;
-				}
-			}
-		} else if (discountType.equals("validityPeriod")) {
-			LocalDate today = LocalDate.now();
-			Boolean withinValidityPeriod = (!today.isBefore(discount.startDate)) && (today.isBefore(discount.endDate));
-			if (withinValidityPeriod == true) {
-				return price - discount.absoluteValue;
-			}
-		} else if (discountType.equals("shoppingCart")) {
+	public static int applyAbsoluteDiscount(int price, Discount discount) {
+		return price - discount.absoluteValue;
+	}
+
+	public static int applyPercentageDiscount(int price, Discount discount) {
+		return price - (price * discount.absoluteValue / 100);
+	}
+
+	public static int applyValidityPeriodDiscount(int price, Discount discount, LocalDate date) {
+		Boolean withinValidityPeriod = (!date.isBefore(discount.startDate)) && (date.isBefore(discount.endDate));
+		if (withinValidityPeriod == true) {
 			return price - discount.absoluteValue;
 		}
 		return 0;
 	}
+
+	public static int applyProdcutSpecificDiscount(int price, Discount discount, String productName) {
+		for (int i = 0; i < discount.applicableProducts.length; i++) {
+			if (discount.applicableProducts[i].equals(productName)) {
+				return price - discount.absoluteValue;
+			}
+		}
+		return 0;
+	}
+
+	public static int applyShoppingCartDiscount(int productPrice1, int productPrice2, int productPrice3,
+			Discount discount) {
+		int subtotal = productPrice1 + productPrice2 + productPrice3;
+		return subtotal - discount.absoluteValue;
+	}
+
 }
--- src/test/java/learning/DiscountCheckerTest.java
+++ src/test/java/learning/DiscountCheckerTest.java
@@ -1,68 +1,61 @@
 package learning;
 
+import java.time.LocalDate;
+
 import junit.framework.TestCase;
 
 public class DiscountCheckerTest extends TestCase {
 
 	public void testAbsolute() {
 		// Given
-		String discountType = "absolute";
 		int productPrice = 500;
 		Discount discount = new Discount(200);
-		String productName = "";
 		// When
-		int finalPrice = DiscountChecker.applyDiscount(discountType, productPrice, discount, productName);
+		int finalPrice = DiscountChecker.applyAbsoluteDiscount(productPrice, discount);
 		// Then
 		assertEquals(300, finalPrice);
 	}
 
 	public void testPercentage() {
 		// Given
-		String discountType = "percentage";
 		int productPrice = 500;
 		Discount discount = new Discount(20);
-		String productName = "";
 		// When
-		int finalPrice = DiscountChecker.applyDiscount(discountType, productPrice, discount, productName);
+		int finalPrice = DiscountChecker.applyPercentageDiscount(productPrice, discount);
 		// Then
 		assertEquals(400, finalPrice);
 	}
 
 	public void testValidityPeriod() {
 		// Given
-		String discountType = "validityPeriod";
 		int productPrice = 1000;
 		Discount discount = new Discount(200);
-		String productName = "";
+		LocalDate date = LocalDate.now();
 		// When
-		int finalPrice = DiscountChecker.applyDiscount(discountType, productPrice, discount, productName);
+		int finalPrice = DiscountChecker.applyValidityPeriodDiscount(productPrice, discount, date);
 		// Then
 		assertEquals(800, finalPrice);
 	}
 
 	public void testProductSpecific() {
 		// Given
-		String discountType = "prodcutSpecific";
 		int productPrice = 500;
 		Discount discount = new Discount(200);
 		String productName = "Latitude";
 		// When
-		int finalPrice = DiscountChecker.applyDiscount(discountType, productPrice, discount, productName);
+		int finalPrice = DiscountChecker.applyProdcutSpecificDiscount(productPrice, discount, productName);
 		// Then
 		assertEquals(300, finalPrice);
 	}
 
 	public void testDiscountOnShoppingCart() {
 		// Given
-		String discountType = "shoppingCart";
 		int productPrice1 = 1000;
 		int productPrice2 = 1000;
 		int productPrice3 = 1000;
-		int subtotal = productPrice1 + productPrice2 + productPrice3;
 		Discount discount = new Discount(500);
-		String productName = "";
 		// When
-		int finalPrice = DiscountChecker.applyDiscount(discountType, subtotal, discount, productName);
+		int finalPrice = DiscountChecker.applyShoppingCartDiscount(productPrice1, productPrice2, productPrice3, discount);
 		// Then
 		assertEquals(2500, finalPrice);
 	}
